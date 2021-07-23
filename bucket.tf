1853326cc2f84938d997161343d17438c441a79f
Nageswara Rao <nageshraoj@gmail.com>
2021-07-23 15:18:36 +0530

    Inital commit

---
+++ bucket.tf
@@ -0,0 +1,29 @@
+resource "aws_s3_bucket" "s3bcuket" {
+  bucket = var.bucket_name
+  acl = "public-read" 
+  versioning {
+    enabled = true
+  }
+  website {
+    index_document = "index.html"
+  }
+  policy = jsonencode({
+    "Id" : "Policy1626177586878",
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "Stmt1626177585820",
+        "Action" : [
+          "s3:GetObject"
+        ],
+        "Effect" : "Allow",
+        "Resource" : "arn:aws:s3:::${var.bucket_name}/*",
+        "Principal" : "*"
+      }
+    ]
+  })
+
+  provisioner "local-exec" {
+    command = "aws s3 cp ${var.app_folder}/. s3://#{var.bucket_name} --recursive"
+  }
+}
---
+++ inputvalues.tf
@@ -0,0 +1,14 @@
+variable "app_region" {
+  description = "Region for the resources"
+  type = string
+}
+
+variable "bucket_name" {
+  description = "Bucket Name"
+  type = string
+}
+
+variable "app_folder" {
+  description = "Folder in which stand alone website is located"
+  type = string
+}
